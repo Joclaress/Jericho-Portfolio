6fef9cf1548870e1eeeb007ee277688a4e261335
Oclares <jerichooclares009@gmail.com>
2023-08-16 23:54:28 +0800

    Updates

---
+++ precache-manifest.d2c9c13555f65a91cdf39a7a72f7c742.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "603ce05675629c35c0c3",
+    "url": "/jerichooclares/static/css/main.e3f7b5bd.chunk.css"
+  },
+  {
+    "revision": "603ce05675629c35c0c3",
+    "url": "/jerichooclares/static/js/main.603ce056.chunk.js"
+  },
+  {
+    "revision": "11419f9ab822d4f34c57",
+    "url": "/jerichooclares/static/js/1.11419f9a.chunk.js"
+  },
+  {
+    "revision": "7954fd54a8248e99ffad",
+    "url": "/jerichooclares/static/css/2.acb7c108.chunk.css"
+  },
+  {
+    "revision": "7954fd54a8248e99ffad",
+    "url": "/jerichooclares/static/js/2.7954fd54.chunk.js"
+  },
+  {
+    "revision": "5d278da570630e556093",
+    "url": "/jerichooclares/static/js/runtime~main.5d278da5.js"
+  },
+  {
+    "revision": "3c5af5c7cac5d6de4a5fe3bfffb83351",
+    "url": "/jerichooclares/static/media/curve-hr.3c5af5c7.svg"
+  },
+  {
+    "revision": "7b619e48b8e04799dbfcb6c8f275c7b9",
+    "url": "/jerichooclares/static/media/logo.7b619e48.svg"
+  },
+  {
+    "revision": "8f2099e49949d03d4bc53499326bdd56",
+    "url": "/jerichooclares/static/media/Resume.8f2099e4.pdf"
+  },
+  {
+    "revision": "2f07d5bc088be3d3eb80075dcdfd8e72",
+    "url": "/jerichooclares/static/media/profile1.2f07d5bc.jpg"
+  },
+  {
+    "revision": "5acd6559e69460948e59b915a6269aa0",
+    "url": "/jerichooclares/static/media/csharp.5acd6559.svg"
+  },
+  {
+    "revision": "99d0b7b12c122e43b5eab231fdee007c",
+    "url": "/jerichooclares/static/media/android.99d0b7b1.svg"
+  },
+  {
+    "revision": "4bea42ef2568375148069b0033e1a66b",
+    "url": "/jerichooclares/static/media/git.4bea42ef.svg"
+  },
+  {
+    "revision": "c10c6d6d8ee3fd097f627869a1757658",
+    "url": "/jerichooclares/static/media/react.c10c6d6d.svg"
+  },
+  {
+    "revision": "b9b9c1bac9f4b84c93f96ff6ae4ec74c",
+    "url": "/jerichooclares/static/media/springboot.b9b9c1ba.svg"
+  },
+  {
+    "revision": "3ae8201441977339d540203a29a534e8",
+    "url": "/jerichooclares/static/media/gitlab.3ae82014.svg"
+  },
+  {
+    "revision": "7bad7166a26bbc3c96ccf1c0907c0dcd",
+    "url": "/jerichooclares/static/media/oraclesql.7bad7166.svg"
+  },
+  {
+    "revision": "35571941b86d74e87bd913f27f4624ba",
+    "url": "/jerichooclares/static/media/mysql.35571941.svg"
+  },
+  {
+    "revision": "60334b45adcc24d76da309b9ad8a48a8",
+    "url": "/jerichooclares/static/media/MSSQL.60334b45.svg"
+  },
+  {
+    "revision": "84045370a85dab3ef611652be26eca7d",
+    "url": "/jerichooclares/static/media/PS.84045370.svg"
+  },
+  {
+    "revision": "19682969a47a7304fab712d4b6d7c61c",
+    "url": "/jerichooclares/static/media/mulesoft.19682969.svg"
+  },
+  {
+    "revision": "17a95894316279f734f7b746c9481ab3",
+    "url": "/jerichooclares/static/media/certificate.17a95894.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/jerichooclares/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/jerichooclares/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/jerichooclares/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/jerichooclares/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "28a4d9db13c0c69029b55c63f135009e",
+    "url": "/jerichooclares/index.html"
+  }
+];
